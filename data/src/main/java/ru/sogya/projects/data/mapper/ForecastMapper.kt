23f0bfc85c42112d7a5f53d6a94b84c0452c6b4e
Timofey Sorokin <Ghost.Spartan36@outlook.com>
2022-06-30 22:42:41 +0300

    added mapper form models to entities

---
+++ data/src/main/java/ru/sogya/projects/data/mapper/ForecastMapper.kt
@@ -0,0 +1,10 @@
+package ru.sogya.projects.data.mapper
+
+import ru.sogya.projects.data.entity.ForecastEntity
+import ru.sogya.projects.domain.models.Forecast
+
+class ForecastMapper {
+    fun toForecastModel(forecast: Forecast): ForecastEntity{
+        TODO("return ForecastEntity")
+    }
+}
