51d9fc0cdb74f9fba333c05055a3c76ed3527e46
Timofey Sorokin <Ghost.Spartan36@outlook.com>
2022-06-30 22:00:11 +0300

    added function realization

--- data/src/main/java/ru/sogya/projects/data/repository/ForecastRrepositoryImpl.kt
+++ data/src/main/java/ru/sogya/projects/data/repository/ForecastRrepositoryImpl.kt
@@ -1,11 +1,31 @@
 package ru.sogya.projects.data.repository
 
-import retrofit2.http.GET
+import android.util.Log
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
+import ru.sogya.projects.data.api.NetworkService
 import ru.sogya.projects.domain.models.Forecast
 import ru.sogya.projects.domain.repositories.ForecastRepository
 
 class ForecastRrepositoryImpl : ForecastRepository {
-    override suspend fun getForecastForMyLocation(): Forecast {
-        @GET("./data/2.5/weather")
+
+    private val forecastApi = NetworkService.retrofitService()
+    private lateinit var forecast: Forecast
+    override fun getForecastForMyLocation(lat: Double, lon: Double): Forecast {
+        forecastApi.getForecast(lat, lon)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(object : DisposableSingleObserver<Forecast>() {
+                override fun onSuccess(t: Forecast) {
+                    forecast = t
+                }
+
+                override fun onError(e: Throwable) {
+                    Log.e("ERROR", e.message.toString())
+                }
+            })
+        return forecast
     }
+
 }
