96ba69111a7f2f20d887141ce784d243063143c7
SogYa <Ghost.Spartan36@gmail.com>
2022-07-18 23:14:22 +0300

    added forecast repository implementation

---
+++ data/src/main/java/ru/sogya/projects/data/repository/ForecastRepositoryImpl.kt
@@ -0,0 +1,12 @@
+package ru.sogya.projects.data.repository
+
+import io.reactivex.Single
+import ru.sogya.projects.data.api.NetworkService
+import ru.sogya.projects.domain.models.Forecast
+import ru.sogya.projects.domain.repositories.ForecastRepository
+
+class ForecastRepositoryImpl : ForecastRepository {
+    override fun getForecastForMyLocation(lat: Double, lon: Double): Single<Forecast> {
+        return NetworkService.retrofitService().getForecast(lat, lon)
+    }
+}
