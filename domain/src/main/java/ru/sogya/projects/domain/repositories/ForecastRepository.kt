dada27f059311dad340f2236ac11efb6f467fdaa
SogYa <Ghost.Spartan36@gmail.com>
2022-07-18 23:13:01 +0300

    changed returnable type

--- domain/src/main/java/ru/sogya/projects/domain/repositories/ForecastRepository.kt
+++ domain/src/main/java/ru/sogya/projects/domain/repositories/ForecastRepository.kt
@@ -1,9 +1,10 @@
 package ru.sogya.projects.domain.repositories
 
+import io.reactivex.Single
 import ru.sogya.projects.domain.models.Forecast
 
 interface ForecastRepository {
 
-    fun getForecastForMyLocation(lat: Double, lon: Double): Forecast
+    fun getForecastForMyLocation(lat: Double, lon: Double): Single<Forecast>
 
 }
