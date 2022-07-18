7f2f3ea400ebfd4bd0888a20bd1c6a1590f68680
SogYa <Ghost.Spartan36@gmail.com>
2022-07-18 23:12:24 +0300

    changed variable types

--- data/src/main/java/ru/sogya/projects/data/entity/ForecastEntity.kt
+++ data/src/main/java/ru/sogya/projects/data/entity/ForecastEntity.kt
@@ -1,20 +1,59 @@
 package ru.sogya.projects.data.entity
 
 import com.google.gson.annotations.SerializedName
+import ru.sogya.projects.domain.models.*
 
 
 data class ForecastEntity(
     @SerializedName("weather")
-    val weatherList: List<ForecastWeatherEntity>,
+    val weather: ForecastWeatherEntity,
     @SerializedName("main")
-    val temperatureList: List<ForecastTemperatureEntity>,
+    val temperature: ForecastTemperatureEntity,
     val visibility: Long,
     @SerializedName("wind")
-    val windList: List<ForecastWindEntity>,
+    val wind: ForecastWindEntity,
     @SerializedName("dt")
     val time: Long,
     @SerializedName("sys")
-    val sysList: List<ForecastSysEntity>,
+    val sys: ForecastSysEntity,
     @SerializedName("name")
     val cityName: String
-)
+) {
+    companion object {
+        fun fromForecastDomain(forecast: Forecast): ForecastEntity =
+            ForecastEntity(
+                fromWeatherDomain(forecast.weather),
+                fromTemeratureDomain(forecast.temperature),
+                forecast.visibility,
+                fromWindDomain(forecast.wind),
+                forecast.time,
+                fromSysDomain(forecast.sys),
+                forecast.cityName
+            )
+
+        fun fromWeatherDomain(forecastWeather: ForecastWeather): ForecastWeatherEntity =
+            ForecastWeatherEntity(
+                forecastWeather.weatherType, forecastWeather.description
+            )
+
+        fun fromTemeratureDomain(forecastTemperature: ForecastTemperature): ForecastTemperatureEntity =
+            ForecastTemperatureEntity(
+                forecastTemperature.temperature,
+                forecastTemperature.feelsLikeTemperature,
+                forecastTemperature.minimalTemperature,
+                forecastTemperature.maximalTemperature,
+                forecastTemperature.pressure,
+                forecastTemperature.humidity
+            )
+
+        fun fromWindDomain(forecastWind: ForecastWind): ForecastWindEntity =
+            ForecastWindEntity(
+                forecastWind.speed, forecastWind.direction
+            )
+
+        fun fromSysDomain(forecastSys: ForecastSys): ForecastSysEntity =
+            ForecastSysEntity(
+                forecastSys.countryCode, forecastSys.sunriseTime, forecastSys.sunsetTime
+            )
+    }
+}
