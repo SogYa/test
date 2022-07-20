fd523ea360f3b81809e304e9f1d37b1009eb94e1
SogYa <Ghost.Spartan36@gmail.com>
2022-07-20 22:21:49 +0300

    added mapper function

--- data/src/main/java/ru/sogya/projects/data/entity/ForecastEntity.kt
+++ data/src/main/java/ru/sogya/projects/data/entity/ForecastEntity.kt
@@ -3,41 +3,51 @@ package ru.sogya.projects.data.entity
 import com.google.gson.annotations.SerializedName
 import ru.sogya.projects.domain.models.*
 
-
 data class ForecastEntity(
     @SerializedName("weather")
-    val weather: ForecastWeatherEntity,
+    val weatherList: List<ForecastWeatherEntity>,
     @SerializedName("main")
     val temperature: ForecastTemperatureEntity,
     val visibility: Long,
     @SerializedName("wind")
-    val wind: ForecastWindEntity,
+    val windList: ForecastWindEntity,
     @SerializedName("dt")
     val time: Long,
     @SerializedName("sys")
-    val sys: ForecastSysEntity,
+    val sysList: ForecastSysEntity,
     @SerializedName("name")
     val cityName: String
 ) {
     companion object {
-        fun fromForecastDomain(forecast: Forecast): ForecastEntity =
-            ForecastEntity(
-                fromWeatherDomain(forecast.weather),
-                fromTemeratureDomain(forecast.temperature),
+        //Mapping ForecastEntity(Data) object to ForecastEntity(Domain)
+        fun toForecastDomain(forecast: ForecastEntity): Forecast =
+            Forecast(
+                fromWeatherListData(forecast.weatherList),
+                fromTemeratureData(forecast.temperature),
                 forecast.visibility,
-                fromWindDomain(forecast.wind),
+                fromWindData(forecast.windList),
                 forecast.time,
-                fromSysDomain(forecast.sys),
+                fromSysData(forecast.sysList),
                 forecast.cityName
             )
 
-        fun fromWeatherDomain(forecastWeather: ForecastWeather): ForecastWeatherEntity =
-            ForecastWeatherEntity(
+        //Map weatherList from domain to weatherList from data
+        private fun fromWeatherListData(forecastWeather: List<ForecastWeatherEntity>): List<ForecastWeather> {
+            val listOfWeatherEntities = mutableListOf<ForecastWeather>()
+            for (forecasts in forecastWeather) {
+                listOfWeatherEntities.add(fromWeatherEntityData(forecasts))
+            }
+            return listOfWeatherEntities
+        }
+
+        private fun fromWeatherEntityData(forecastWeather: ForecastWeatherEntity): ForecastWeather =
+            ForecastWeather(
                 forecastWeather.weatherType, forecastWeather.description
             )
 
-        fun fromTemeratureDomain(forecastTemperature: ForecastTemperature): ForecastTemperatureEntity =
-            ForecastTemperatureEntity(
+
+        private fun fromTemeratureData(forecastTemperature: ForecastTemperatureEntity): ForecastTemperature =
+            ForecastTemperature(
                 forecastTemperature.temperature,
                 forecastTemperature.feelsLikeTemperature,
                 forecastTemperature.minimalTemperature,
@@ -46,13 +56,13 @@ data class ForecastEntity(
                 forecastTemperature.humidity
             )
 
-        fun fromWindDomain(forecastWind: ForecastWind): ForecastWindEntity =
-            ForecastWindEntity(
+        private fun fromWindData(forecastWind: ForecastWindEntity): ForecastWind =
+            ForecastWind(
                 forecastWind.speed, forecastWind.direction
             )
 
-        fun fromSysDomain(forecastSys: ForecastSys): ForecastSysEntity =
-            ForecastSysEntity(
+        private fun fromSysData(forecastSys: ForecastSysEntity): ForecastSys =
+            ForecastSys(
                 forecastSys.countryCode, forecastSys.sunriseTime, forecastSys.sunsetTime
             )
     }
