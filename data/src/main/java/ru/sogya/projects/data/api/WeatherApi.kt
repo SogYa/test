511acf93598b8fdfd0ef24c3a571ea37aed1e19b
SogYa <Ghost.Spartan36@gmail.com>
2022-07-20 22:21:17 +0300

    added units parameter

--- data/src/main/java/ru/sogya/projects/data/api/WeatherApi.kt
+++ data/src/main/java/ru/sogya/projects/data/api/WeatherApi.kt
@@ -3,9 +3,10 @@ package ru.sogya.projects.data.api
 import io.reactivex.Single
 import retrofit2.http.POST
 import retrofit2.http.Query
+import ru.sogya.projects.data.entity.ForecastEntity
 import ru.sogya.projects.domain.models.Forecast
 
 interface WeatherApi {
-    @POST("weather?appid=3fa835e02db74ada88227026f9ea6a31")
-    fun getForecast(@Query("lat") lat: Double, @Query("lon") lon: Double): Single<Forecast>
+    @POST("weather?appid=3fa835e02db74ada88227026f9ea6a31&units=metric")
+    fun getForecast(@Query("lat") lat: Double, @Query("lon") lon: Double): Single<ForecastEntity>
 }
